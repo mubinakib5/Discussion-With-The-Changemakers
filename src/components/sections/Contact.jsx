7d087a49896727b5e1a12da79889f9cc40ce6be9
mubinakib5 <mubinakib027@gmail.com>
2025-06-01 16:42:10 +0600

    Footer Added

--- src/components/sections/Contact.jsx
+++ src/components/sections/Contact.jsx
@@ -247,7 +247,7 @@ const Contact = () => {
           </motion.form>
         </div>
       </div>
-      <p className="flex items-center justify-center gap-1 mt-4">
+      <p className="flex items-center justify-center gap-1 mt-10">
         Made with{" "}
         <svg
           className="w-4 h-4 text-red-500 animate-pulse"
@@ -256,7 +256,15 @@ const Contact = () => {
         >
           <path d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z" />
         </svg>{" "}
-        by Decor's Digital.
+        by{" "}
+        <a
+          href="https://www.facebook.com/profile.php?id=100091279212121"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="text-brand-primary hover:underline"
+        >
+          Decor's Digital.
+        </a>
       </p>
     </section>
   );
